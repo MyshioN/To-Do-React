14bbc9c8b9d02bc8a1e08afe358ee5360fe305a7
Szymon Kolarz <szymonkolarz@MacBook-Air-Szymon.local>
2022-11-05 17:26:43 +0100

    Finall version of the app ToDoList

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
+import AddTask from './components/AddTask.jsx';
 import Header from "./components/Header.jsx"
 import Tasks from "./components/Tasks.jsx"
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'SIuuuuuu',
-      day: 'Siuuu',
-      reminder: false,
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -22,8 +23,9 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('Everything done')}
     </div>
   )
--- todoapp/src/components/Header.jsx
+++ todoapp/src/components/Header.jsx
@@ -1,10 +1,9 @@
-import Button from "./Button"
+
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
         <h1 style={{ color: 'blue' }}>To-Do-List</h1>
-        <Button />
     </header>
   )
 }
